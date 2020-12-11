a43de775156d0a547d49b53ac8fdf86d2db13f67
Daniel <dbuehler1@my.wctc.edu>
2020-12-10 19:04:53 -0600

    updated features

--- cars.js
+++ cars.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+    //events
     $("#OrderCar").hide();
     $("#Catalog").hide();
     $("#Cart").hide();
@@ -13,12 +14,68 @@ $(document).ready(function () {
     $("#btnFresh").click(addToCart);
     $("#btnNeon").click(addToCart);
     $("#btnDisplay").click(displayCart);
+    $("#btnClear").click(ClearCart);
+    $("#CarOrderForm").submit(Car);
 
 
 
+
+//function figures out what the name of the person is along with requiring certain fields
+    // At the end it will print out details about the order along with the person's name.
+    function Car(){
+        event.preventDefault();
+        var CarType = "";
+        var Color = "";
+        var firstname = $("#FName").val();
+        var lastname = $("#LName").val();
+
+        var CarTotal = 0;
+        if($("#red:checked").length == 1){
+            Color = "red";
+        }
+        else if($("#blue:checked").length == 1){
+            Color = "Blue";
+        }
+        else if($("#green:checked").length == 1){
+            Color = "Green";
+        }
+
+        if($("#Normal:checked").length == 1){
+            CarTotal = 15000;
+            CarType = "Car";
+        }
+        else if($("#Truck:checked").length == 1){
+            CarTotal = 25000;
+            CarType = "Truck";
+        }
+        else if($("#sport:checked").length == 1){
+            CarTotal = 35000;
+            CarType = "Sports Car";
+        }
+        if($("#spareTire:checked").length == 1){
+            CarTotal = CarTotal + 250;
+        }
+        //validation
+        if(firstname.length < 1 || lastname.length < 1){
+            $("#CarTotal").text("!!Please Enter Required Fields!!");
+            $("#orderDesc").text("");
+        }
+        else{
+            $("#CarTotal").text("Total: $" + CarTotal.toFixed(2));
+            $("#orderDesc").text("Congrats "+ firstname + " " + lastname + " on your new " + Color + " "
+                + CarType + ", Enjoy!");
+        }
+
+    }
+    //array to hold entries
     var entries = [];
-
-
+    //clear out array and empty div
+    function ClearCart(){
+        entries = [];
+        $("#CartItems").empty();
+        $("#total").text("Total: 0");
+    }
+//displays Home and hides others
     function Home(){
         event.preventDefault();
         $("#Home").show();
@@ -26,6 +83,7 @@ $(document).ready(function () {
         $("#Catalog").hide();
         $("#Cart").hide();
     }
+    //displays OrderCar and hides others
     function OrderCar(){
         event.preventDefault();
         $("#OrderCar").show();
@@ -33,6 +91,7 @@ $(document).ready(function () {
         $("#Catalog").hide();
         $("#Cart").hide();
     }
+    //displays Catalog and hides others
     function Catalog(){
         event.preventDefault();
         $("#Catalog").show();
@@ -40,71 +99,95 @@ $(document).ready(function () {
         $("#OrderCar").hide();
         $("#Cart").hide();
     }
+    //displays Cart and hides others
     function Cart(){
         event.preventDefault();
         $("#Cart").show();
         $("#Home").hide();
         $("#Catalog").hide();
         $("#OrderCar").hide();
+
     }
 
 
 
-
+//takes in information from buttons and from inputs to calculate total and store information in
+    //and object and then stores that object in an array.
     function addToCart() {
+
         var itemQty = 0;
-        var PName = "";
-        /*switch(test){
-            case 1:
+        var price = this.dataset.price;
+        var PName = this.dataset.name;
+        $("#error").text("");
+
+
+        switch(PName){
+            case 'Phone Holder':
                 itemQty = $("#phoneHolder").val();
-                PName = "Phone Holder";
                 break;
-            case 2:
+            case 'Spoiler':
                 itemQty = $("#Spoiler").val();
-                PName = "Spoiler";
                 break;
-            case 3:
+            case 'Subwoofers':
                 itemQty = $("#Subwoofers").val();
-                PName = "Subwoofer";
                 break;
-            case 4:
+            case 'Air Freshener':
                 itemQty = $("#AirFreshener").val();
-                PName = "Air Freshener";
                 break;
-            case 5:
+            case 'Neon Headlights':
                 itemQty = $("#NeonLights").val();
-                PName = "Neon Headlights";
                 break;
-        }*/
+        }
+        //validation to ensure no negatives or 0's are stored
+        if(itemQty < 1)
+        {
+            $("#error").text("Cannot add items with less than 1 QTY");
+        }
+        else{
+            var total = itemQty * price;
+
+            var entry = {
+                ProductName: PName,
+                Price: price,
+                Qty: itemQty,
+                Total: total
+            };
+            entries.push(entry);
+        }
 
 
-        var total = 0;
-
-        var entry = {
-            ProductName: PName,
-            Qty: itemQty,
-            Total: total
-        };
-        entries.push(entry);
 
     }
-
+//Displays all things in cart by printing out details from every object
     function displayCart() {
         event.preventDefault();
         var Total = 0;
+        $("#CartItems").empty();
         for (var x = 0; x < entries.length; x++) {
-            var newPName = $("<p>");
-            $("#Cart").append(newPName);
-            newPName.attr("id", 'PName${x}');
-            newPName.text("Product Name: " + entries[x].ProductName);
+            if(entries[x].Qty > 0){
+                //product name
+                var newPName = $("<p>");
+                $("#CartItems").append(newPName);
+                newPName.attr("id", 'PName${x}');
+                newPName.text("Product Name: " + entries[x].ProductName);
+                //quantity
+                var newQty = $("<p>");
+                $("#CartItems").append(newQty);
+                newQty.attr("id", 'QTY${x}');
+                newQty.text("Quantity: " + entries[x].Qty);
+                //price per unit
+                var newPrice = $("<p>");
+                $("#CartItems").append(newPrice);
+                newPrice.attr("id", 'Price${x}');
+                newPrice.text("Per Unit: " + entries[x].Price);
 
-            var newQty = $("<p>");
-            $("#Cart").append(newQty);
-            newQty.attr("id", 'QTY${x}');
-            newQty.text("Quantity: " + entries[x].Qty);
+                Total = Total + entries[x].Total;
+            }
 
 
         }
+        //total and tax
+        $("#total").text("Total: $" + (Total * 1.05).toFixed(2) + "  Tax: $" + (Total * 0.05).toFixed(2));
 
     }
 
